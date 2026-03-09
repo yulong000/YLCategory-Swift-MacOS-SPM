f094d22aaba6deef84a70fdd9751a7f41424acf6
魏宇龙 <weiyulong1987@163.com>
2026-03-09 11:25:32 +0800

    优化

--- Sources/YLCFNotificationManager/YLCFNotificationCenter.swift
+++ Sources/YLCFNotificationManager/YLCFNotificationCenter.swift
@@ -51,7 +51,7 @@ public class YLCFNotificationCenter {
     ///   - userInfo: 发送到信息
     ///   - observer: 回调接收对象，传nil则不会回调
     ///   - handler: 对方收到通知后的回调
-    public func postCFNotification(_ name: Notification.Name, userInfo: [String : Any]? = nil, observer: AnyObject, handler: @escaping ([String : Any]?) -> Void) {
+    public func postCFNotification(_ name: Notification.Name, userInfo: [String : Any]? = nil, observer: AnyObject, callback handler: @escaping ([String : Any]?) -> Void) {
         let info = (isSandbox ? nil : userInfo) as CFDictionary?
         let notificationName = CFNotificationName(name.rawValue as CFString)
         CFNotificationCenterPostNotificationWithOptions(CFNotificationCenterGetDistributedCenter(), notificationName, nil, info, kCFNotificationDeliverImmediately | kCFNotificationPostToAllSessions)
