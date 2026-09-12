76323d5efba91e72ee2c5839c654c56d2eb232a0
魏宇龙 <weiyulong1987@163.com>
2026-09-12 10:50:22 +0800

    fix  user 名字获取

--- Sources/YLKit/User.swift
+++ Sources/YLKit/User.swift
@@ -12,19 +12,29 @@ import SystemConfiguration
 public struct User {
     
     /// 访达中显示的用户名 （/Users/xxx中的xxx）
-    public static let name: String = { home.components(separatedBy: "/").last ?? NSUserName() }()
+    public static var name: String { home.components(separatedBy: "/").last ?? NSUserName() }
     /// 访达中的用户目录 /Users/xxx）
-    public static let home: String = {
-        guard let pwd = getpwuid(getuid()),
+    public static var home: String {
+        var uid = getuid()
+        
+        if let userName = SCDynamicStoreCopyConsoleUser(nil, &uid, nil) as String?,
+           !userName.isEmpty,
+           userName.lowercased() != "loginwindow" {
+            // 使用桌面登录用户的uid
+        } else {
+            uid = getuid()
+        }
+        
+        guard let pwd = getpwuid(uid),
               let home = pwd.pointee.pw_dir else {
-            return "/Users/\(NSUserName())"
+            return NSHomeDirectory()
         }
         return String(cString: home)
-    }()
+    }
     /// 全名（登录时显示的名字）
-    public static let fullName: String = NSFullUserName()
+    public static var fullName: String { NSFullUserName() }
     /// 登录名（活动监视器中的用户名）
-    public static let loginName: String = NSUserName()
+    public static var loginName: String { NSUserName() }
     /// 是否是用户登录窗口
     public static var isLoginWindow: Bool {
         guard let userName = SCDynamicStoreCopyConsoleUser(nil, nil, nil) as? String else {
@@ -32,5 +42,4 @@ public struct User {
         }
         return userName.lowercased() == "loginwindow"
     }
-    
 }
